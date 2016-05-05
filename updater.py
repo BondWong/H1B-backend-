8857d359737823ce1b5951f267702a426e149664
Junbang Huang <wongzeonbong@gmail.com>
2016-05-05 13:18:01 -0400

    introduce message queue

--- updater.py
+++ updater.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
+import sys
+from openpyxl import load_workbook
+from concurrent.futures import ThreadPoolExecutor
+from openpyxl.cell import get_column_letter
+import requests
+import json
+from requester import post
+import threading
+import queue
 
-if __name__ == "__main__":
-    import sys
-    from openpyxl import load_workbook
-    from concurrent.futures import ThreadPoolExecutor
-    from openpyxl.cell import get_column_letter
-    import requests
-    import json
-    from requester import post
-    
-    # open excel
-    filePath = sys.argv[1]
-    url = sys.argv[2]
-    tasks = {}
-    wb = load_workbook(filePath, read_only=True)
-    ws = wb.worksheets[0]
+class ProducerThread(threading.Thread):
+    def __init__(self):
+        super(ProducerThread, self).__init__()
 
-    # read data into memory and send 
-    data = []
-    i = 2
-    with ThreadPoolExecutor(128) as pool:
+    def run(self):
+        data = []
+        i = 2
         for row in ws.iter_rows(row_offset = 2):
-            i += 1
             d = {"name": "", "contact": "", "state": "", "city": "", "zipCode": "", "street": "", "title": "", "salary": "", "visaType": "H-1B", "workState": "", "workCity":"",  "workZipCode": ""}
+            i += 1
             for cell in row:
                 if not cell.value:
                     continue
@@ -55,9 +51,28 @@ if __name__ == "__main__":
                 elif col == "E" and cell.value != "H-1B":
                     continue;
             
-            data.append(d)
-            if len(data) >= 1000:
-                pool.submit(post, url, data)
-                data = []
-                print(i)
+                data.append(d)
+                if len(data) >= 1000:
+                    while queue.full():
+                        pass
+                    queue.put(data)
+                    data = []
 
+if __name__ == "__main__":
+    filePath = sys.argv[1]
+    url = sys.argv[2]
+    tasks = {}
+    wb = load_workbook(filePath, read_only=True)
+    ws = wb.worksheets[0]
+    queue = queue.Queue(50)
+
+    producer = ProducerThread()
+    producer.start()
+
+    with ThreadPoolExecutor(32) as pool:
+        while(True):
+            if(not queue.empty()):
+                data = queue.get()
+                pool.submit(post, url, data)
+
+    
